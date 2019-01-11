52b7d52937ac86b17241cb40574d1c712b81994f
Yuriy Zhmurko <777zhm@gmail.com>
2019-01-11 09:56:34 +0200

    before 110119

--- js/script.js
+++ js/script.js
@@ -81,6 +81,35 @@
 
 
 
+
+
+	$(window).mousemove(function(e) {
+		var change;
+		var xpos=e.clientX;
+		var ypos=e.clientY;
+		var y_bottom_pos = $(window).height() - e.clientY;
+		var left= change*20;
+		var  xpos=xpos*2;ypos=ypos*2;
+		console.log(xpos);
+		$('.satelite_artbord .satelite_mouse').css('bottom', 'calc( -3vw ' + ' + ' + (y_bottom_pos/25) + 'px )' );
+
+		$('.satelite_artbord .satelite_server').css('bottom', 'calc( 12vw ' + ' + ' + (y_bottom_pos/35) + 'px )').css('left', 'calc( 42vw ' + ' + ' + (xpos/40) + 'px )' );
+
+		$('.satelite_artbord .satelite_gps').css('bottom', 'calc( 5vw ' + ' + ' + (y_bottom_pos/45) + 'px )').css('left',  (xpos/45) + 'px' );
+		
+
+
+		$('.man').css('right',(( 0+(xpos/80))+"px"));
+
+	});
+
+	// Start Backtotop button	      
+	$('.backtotop_button').click(function(){ 
+        $('html,body').animate({ scrollTop: 0 } );
+        return false; 
+	});
+	// End Backtotop button
+
 	});
 
 })( jQuery );
